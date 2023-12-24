b56ebdf8ca378973111fb282b9699ae3e727fc63
RoaringCow <ersandemircan57@gmail.com>
2023-12-24 12:02:31 +0300

    stable video

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod video;
 use video::process_video;
 use std::thread;
+use std::io::{stdout, Write};
 
 const BLACK: (u8, u8, u8) = (0, 0, 0);
 const RED: (u8, u8, u8) = (255, 0, 0);
@@ -32,18 +33,24 @@ impl ChangableColor for str {
 
 
 fn main() {
-    let delay = std::time::Duration::from_millis(20);
+    let delay = std::time::Duration::from_millis(1000 / 30);
 
-    let frames = process_video("/home/ersan/tintrs/sonic.mp4", 10);
+    let frames = process_video("/home/ersan/tintrs/nevergonnagiveyouup.mp4", 5);
 
     for frame in frames {
-        print!("{}[2J", 27 as char);
-        thread::sleep(std::time::Duration::from_millis(10));
+        print!("\x1B[2J\x1B[1;1H");
+
+        let mut buffer = String::new();
+
         for row in frame {
             for pixel in row {
-                print!("{}", "  ".background_color((pixel.r, pixel.g, pixel.b)));
+                buffer.push_str(&"  ".background_color((pixel.r, pixel.g, pixel.b)).to_string());
             }
-            println!();
+            buffer.push_str("\n");
         }
+        print!("{}", buffer);
+        stdout().flush().unwrap();
+
+        thread::sleep(delay);
     }
 }
